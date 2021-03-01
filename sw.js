914f16b4c903079aca94855dbf719233f2ac5d6d
smallpotato47 <79006104+smallpotato47@users.noreply.github.com>
2021-03-01 08:24:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,13 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1-3.5';
+const PRECACHE = 'precache-v1-3.6';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
  'index.html',
  './', // Alias for index.html
+ 'welcome.html',
  'base64.js',
  'confirm.html',
  'font/Rubik-VariableFont_wght.ttf',
